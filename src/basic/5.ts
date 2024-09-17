78e5525af0125c35c0ba75fe8c576536d2508e4d
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-09-17 22:43:17 +0200

    basic_generic

--- src/basic/5.ts
+++ src/basic/5.ts
@@ -17,3 +17,22 @@ const isWeekend = (day: DayOfWeek): boolean => {
   }
   return false;
 };
+
+// type DayStatuses = Record<DayOfWeek, boolean>;
+
+// const dayStatuses: DayStatuses = {
+//   [DayOfWeek.Monday]: false,
+//   [DayOfWeek.Tuesday]: false,
+//   [DayOfWeek.Wednesday]: false,
+//   [DayOfWeek.Thursday]: false,
+//   [DayOfWeek.Friday]: false,
+//   [DayOfWeek.Saturday]: true,
+//   [DayOfWeek.Sunday]: true,
+// };
+
+// const isWeekend = (day: DayOfWeek): boolean => {
+//   return dayStatuses[day];
+// };
+
+// console.log(isWeekend(DayOfWeek.Monday));
+// console.log(isWeekend(DayOfWeek.Saturday));
